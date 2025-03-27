513bfaaea875f7437b59168dcef9e67a261f07c5
Darpan-Lab <darpankabir9@gmail.com>
2025-03-28 02:10:18 +0600

    final

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ const currentDate = new Date().toLocaleDateString();
 const currentTime = new Date().toLocaleTimeString([], {
     hour12: true
 });
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const day = weekday[new Date().getDay()];
+document.getElementById('day').innerText = day
 let taskAssigned = 6;
 let Point = 21;
 document.getElementById('time').innerText = currentDate;
@@ -19,6 +22,7 @@ document.getElementById('theme-btn').addEventListener('click', function(){
 const buttons = document.querySelectorAll('.complete-btn');
 for(const btn of buttons){
     btn.addEventListener('click', function(event){
+        alert("Board Updated Successfully")
         taskAssigned = taskAssigned - 1;
         document.getElementById('task-assigned').innerHTML = taskAssigned;
         Point = Point + 1
@@ -27,7 +31,18 @@ for(const btn of buttons){
         event.target.setAttribute("disabled", true)
         const title = event.target.parentNode.parentNode.querySelector('h2').innerText
         const msg = document.createElement('p')
+        msg.classList.add('activity-messages')
         msg.innerText = `You have Complete The Task ${title} at ${currentTime}`
-        document.getElementById('activity-msg').appendChlid(msg)
+        document.getElementById('activity-msg').appendChild(msg)
+        if(taskAssigned == 0){
+            alert("Congrates! You have Completed all the current tasks")
+        }
     })
 }
+document.getElementById('clear-btn').addEventListener('click', function(){
+    document.getElementById('activity-msg').innerHTML = ""
+})
+
+document.getElementById('discover').addEventListener('click', function(){
+    window.location.href="../blog.html"
+})
